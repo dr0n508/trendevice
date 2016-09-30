ae08410806e1c8a51c0e8aa6c5e8795570d3fcf3
Andriy Kornienko <kornienko.andriy@gmail.com>
2016-09-30 15:51:49 +0300

    add custome-checkbox-conferma

--- js/main.js
+++ js/main.js
@@ -153,6 +153,23 @@ $(document).ready(function () {
         }
     });
 
+    /*********************/
+
+    $('#accept-terms').change(function() {
+        if($('#accept-terms').prop("checked")) {
+            $('#conferma-btn').removeClass('disable');
+        }
+        else {
+            $('#conferma-btn').addClass('disable');
+        }
+    });
+
+
+
+
+
+
+
 
 
 
